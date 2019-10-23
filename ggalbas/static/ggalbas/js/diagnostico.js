5b55ef3de7e7b970cb9b5e9950ee3813b5eafb96
Cristhian Paul Ramirez <criramirez@compumat.cl>
2019-10-23 13:25:14 -0300

    crear funcion ajax para enviar en el ejercicio las respuestas del alumno

--- ggalbas/static/ggalbas/js/diagnostico.js
+++ ggalbas/static/ggalbas/js/diagnostico.js
@@ -1,40 +1,87 @@
 var url;
+
 $('#continuar').on('click', function(){
     url = "portadaVisor";
     $(location).attr('href',url);
 });
+
 $('#vuelveAnteP').on('click', function(){
     url = 'antePortada';
     $(location).attr('href',url);
 });
 
-evaluaEjercicio();
 
 function evaluaEjercicio(){
 
-$('#flecha').on('click', function(){
-    fill1= $('#fill-1').val();
+    $('#flecha').on('click', function(){
 
-    if(fill1==""){
-        $('#ModalEjercicio').modal('show');
-    }
-    else {
-        $.ajax({
-            async: true,
-            type: "POST",
-            url: "guardaRespuesta",
-            dataType: 'json',
-            data: {
-                    fill1: fill1,
-                  },
-            success:function(respuesta){
-                console.log(respuesta);
+        var respuestaAlumno = '';
+        var abecedario=new Array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+
+        var continuar=1;
+        var selecciona_check=0;
+        var selecciona_radio=0;
+
+        if ($('.fill-input').length) { // los campos son de tipo text
+            
+            $(".fill-input").each(function (index) {  
+                if($(this).val()==''){
+                    continuar = 0;
+                    $('#ModalEjercicio').modal('show');
+                    return false;
+                }
+                respuestaAlumno += respuestaAlumno == '' ? $(this).val() : ","+$(this).val();
+            });
+        }
+
+        if ($('.radio-input').length) { // los campos son de tipo radio
+           
+            $(".radio-input").each(function (index) {             
+                if($(this).is(':checked')) {  
+                    selecciona_radio =1;
+                    respuestaAlumno += abecedario[parseInt($(this).attr('id')) -1];
+                }          
+            });
+
+            if(selecciona_radio==0){
+                continuar = 0;
+                $('#ModalEjercicio').modal('show');
             }
+        }
 
-        });
-    }
-});
+        if ($('.checkbox-input').length) { // los campos son de tipo checkbox
+
+            $(".checkbox-input").each(function (index) {             
+                if($(this).is(':checked')) {  
+                    selecciona_check =1; 
+                    respuestaAlumno += abecedario[parseInt($(this).attr('id')) -1];
+                }         
+            });
+        
+            if(selecciona_check==0){
+                continuar = 0;
+                $('#ModalEjercicio').modal('show');
+            }
+        }
+ 
+        if (continuar==1){
+
+            $.ajax({
+                async: true,
+                type: "POST",
+                url: "guardaRespuesta",
+                dataType: 'json',
+                data: { 'respuestaAlumno': respuestaAlumno },
+                success:function(respuesta){
+                    url = 'visorActividades';
+                    $(location).attr('href',url);
+                }
+
+            });
+        }
+        
+    });
 
 }
 
-
+evaluaEjercicio();
