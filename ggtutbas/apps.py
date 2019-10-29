8ef728749628c7841a2b776b780309ff8c8ee1d4
eliezer_anaya <elianaya@compumat.com>
2019-10-29 16:13:07 -0300

    Creación de app Tutor Emat guiado

---
+++ ggtutbas/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GgtutbasConfig(AppConfig):
+    name = 'ggtutbas'
--- proyectoEmat/urls.py
+++ proyectoEmat/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ggalbas/', include('ggalbas.urls')),
+    path('ggtutbas/', include('ggtutbas.urls')),
 ]
