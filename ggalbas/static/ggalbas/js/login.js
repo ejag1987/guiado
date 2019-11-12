3c548615ef9a6e6abe4178422a38fff99cbe8e84
Cristhian Paul Ramirez <criramirez@compumat.cl>
2019-11-12 09:15:49 -0300

    correccion de error en el login

--- ggalbas/static/ggalbas/js/login.js
+++ ggalbas/static/ggalbas/js/login.js
@@ -1,44 +1,131 @@
+
+$( document ).ready(function() {
+
 $('#entrar').attr("disabled", true);
 
-var url = jQuery(location).attr('href');
+ $.getJSON( "https://api.ipify.org?format=json", function(data) {
+            $("#ip").val(data.ip);
+         })
+         .fail(function() {
+            $.ajax( "obtenerIp" )
+                  .done(function(ip) {
+                   $("#ip").val( ip);
+                  });
+         });
 
-function cargaAnimacion(){
-  $('#loader').html('<img src="/static/ggalbas/images/loader.gif" alt="loader">').fadeIn(1000);
- }
+$('#validador').keyup(function(e){
 
-function quitarAnimacion(){
-  $('#loader').html('<img src="/static/ggalbas/images/loader.gif" alt="loader">').fadeOut(1000);
- }
+    var rutCompleto = $("#rut").val().trim()+"-"+$("#validador").val().trim();
+    $('#errorRut').html('').fadeOut(1000);
+    $('#errorPass').html('').fadeOut(1000);
+    $('#errorCaptcha').html('').fadeOut(1000);
 
- function mostrarPassword(){
-$('#input-password').removeClass('oculto');
- }
+    if($("#rut").val().trim()=="" || $("#validador").val().trim()=="" ){
+        //$('#errorRut').html('<span>rut o validador vacios</span>').fadeIn(1000);
+    }else if(Fn.validaRut(rutCompleto)==false){
+       $('#errorRut').html('<span>Debe ingresar un Rut válido</span>').fadeIn(1000);
+    }else{
 
- function quitarPassword(){
-  $('#input-password').addClass('oculto');
- }
+       cargaAnimacion();
 
- function mostrarCaptcha(){
-  $('#captcha').removeClass('oculto');
-  $('#captcha').css('display', 'flex');
- }
+       $.ajax({
+          async: true,
+          type: "POST",
+          url: "verificaRutIp",
+          dataType: 'json',
+          data: {
+            'ip': $("#ip").val(),
+            'rut': rutCompleto
+          },
+          success: function(respuesta){
+            quitarAnimacion();
+            $('#entrar').removeAttr("disabled");
+            if(respuesta.status=='validado'){
+                  $('#tipoIngreso').val("soloRut");
+                  $('#input-password').addClass('oculto');
+                  $('#captcha').addClass('oculto');
+            }
+            else{
+                  $('#tipoIngreso').val("completo");
+                  $('#input-password').removeClass('oculto');
+                  $('#password').val('');
+                  $('#captcha').removeClass('oculto');
+                  $('#captcha').css('display', 'flex');
+                  $('#texto-captcha').val('');
+            }
+          },
+          error: function(){
+            console.log('hay un error');
+          }
+    });
+    }
 
- function desactivarCaptcha(){
-  $('#captcha').addClass('oculto');
- }
+});
 
- function validaIngresoRut(){
   $('#entrar').on('click', function(e){
     e.preventDefault();
     e.stopImmediatePropagation();
+   if($("#tipoIngreso").val()=="soloRut"){
+       ingresoSoloRut();
+   }else if($("#tipoIngreso").val()=="completo"){
+       ingresoCompleto();
+   }
+
+});
+
+
+
+});
+
+  var Fn = {
+  // Valida el rut con su cadena completa "XXXXXXXX-X"
+  validaRut : function (rutCompleto) {
+    rutCompleto = rutCompleto.replace("‐","-");
+    if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test( rutCompleto ))
+      return false;
+    var tmp   = rutCompleto.split('-');
+    var digv  = tmp[1];
+    var rut   = tmp[0];
+    if ( digv == 'K' ) digv = 'k' ;
+
+    return (Fn.dv(rut) == digv );
+  },
+  dv : function(T){
+    var M=0,S=1;
+    for(;T;T=Math.floor(T/10))
+      S=(S+T%10*(9-M++%6))%11;
+    return S?S-1:'k';
+  }
+}
+
+
+function cargaAnimacion(){
+  $('#loader').fadeIn('fast');
+ }
+
+function quitarAnimacion(){
+  $('#loader').fadeOut('fast');
+ }
+
+function  ingresoSoloRut(){
+   var rutCompleto = $("#rut").val().trim()+"-"+$("#validador").val().trim();
+   $('#errorRut').html('').fadeOut(1000);
+
+  if($('#rut').val().trim() =="" || $('#validador').val().trim() ==""){
+       $('#errorRut').html('<span>rut o validador vacios</span>').fadeIn(1000);
+    }else if(Fn.validaRut(rutCompleto)==false){
+       $('#errorRut').html('<span>Debe ingresar un Rut válido</span>').fadeIn(1000);
+    } else{
+
     cargaAnimacion();
+
     $.ajax({
             async: true,
             type: 'POST',
-            url: url+"ingresoSoloRut",
+            url: "ingresoSoloRut",
             dataType: 'json',
             data: {
-                    rut: $('#rut').val()+-+$('#validador').val() 
+                    rut: $('#rut').val()+-+$('#validador').val()
             },
             success: function (respuesta){
                   quitarAnimacion();
@@ -50,75 +137,43 @@ $('#input-password').removeClass('oculto');
                     }
                   }
             }
-
     });
-  });
-}
 
 
-
-  var Fn = {
-  // Valida el rut con su cadena completa "XXXXXXXX-X"
-  validaRut : function (rutCompleto) {
-    rutCompleto = rutCompleto.replace("‐","-");
-    if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test( rutCompleto ))
-      return false;
-    var tmp   = rutCompleto.split('-');
-    var digv  = tmp[1]; 
-    var rut   = tmp[0];
-    if ( digv == 'K' ) digv = 'k' ;
-    
-    return (Fn.dv(rut) == digv );
-  },
-  dv : function(T){
-    var M=0,S=1;
-    for(;T;T=Math.floor(T/10))
-      S=(S+T%10*(9-M++%6))%11;
-    return S?S-1:'k';
-  }
-}
-
-function validaIngresoCompleto(ip){
-
-$('#entrar').click(function(e){
-    e.preventDefault();
-    e.stopImmediatePropagation();
-    var rut= $('#rut').val();
-    var validador= $('#validador').val();
-
-    if (rut=="" || validador=="" ){
-   // $('#errorRut').html('<span>Debe ingresar un Rut válido</span>').fadeIn(1000); 
     }
-    else{
-       $('#errorRut').fadeOut('slow');
-       if ($('#password').val()==""){
-        $('#errorPass').html('<span> Debe ingresar su contraseña</span>').fadeIn(1000);
-        console.log('sin password');
-        }
-        else{
-          $('#errorPass').fadeOut('slow');
-          if($('#img-captcha').text()!=$('#texto-captcha').val() || $('#texto-captcha').val()=="" ){
-            $('#errorCaptcha').html('<span>Ingrese los números del recuadro naranjo</span>').fadeIn(1000);
-            console.log('captcha diferente');
-          }
-          else{
+
+}
+
+function ingresoCompleto(){
+      var rutCompleto = $("#rut").val().trim()+"-"+$("#validador").val().trim();
+      $('#errorRut').html('').fadeOut(1000);
+      $('#errorPass').html('').fadeOut(1000);
+      $('#errorCaptcha').html('').fadeOut(1000);
+
+    if($('#rut').val().trim() =="" || $('#validador').val().trim() ==""){
+       $('#errorRut').html('<span>rut o validador vacios</span>').fadeIn(1000);
+    }else if(Fn.validaRut(rutCompleto)==false){
+       $('#errorRut').html('<span>Debe ingresar un Rut válido</span>').fadeIn(1000);
+    }else if($('#password').val().trim()==""){
+         $('#errorPass').html('<span> Debe ingresar su contraseña</span>').fadeIn(1000);
+    }else if($('#img-captcha').text()!=$('#texto-captcha').val() || $('#texto-captcha').val().trim()=="" ){
+       $('#errorCaptcha').html('<span>Ingrese los números del recuadro naranjo</span>').fadeIn(1000);
+    }else{
 
             cargaAnimacion();
 
             $.ajax({
             async: true,
             type: 'POST',
-            url: url+"ingresoCompleto",
+            url: "ingresoCompleto",
             dataType: 'json',
             data: {
-                    rut: $('#rut').val()+-+$('#validador').val(),
-                     password: $('#password').val(),
-                     ip: ip
+                    rut: rutCompleto,
+                    password: $('#password').val(),
+                    ip: $("#ip").val(),
             },
             success: function (respuesta){
               quitarAnimacion();
-              $('#errorPass').fadeOut('slow');
-              $('#errorCaptcha').fadeOut('slow');
               if(respuesta.status=='datos alumno ok'){
                     if (respuesta.nuevo){
                         $(location).attr('href',"antePortada");
@@ -131,95 +186,6 @@ $('#entrar').click(function(e){
               }
             }
           });
-          }
-        }
     }
-    });
-}
-
-validaRutIp();
-
-function validaRutIp(){
-
-  $('#rut').on('blur', function(){
-
-    var rut = $(this).val();
-
-    $('#validador').keyup(function(){
-
-      var validador= $(this).val();
-
-      var rutCompleto = rut+'-'+validador;
-
-      if (rut=="" || validador==""){
-
-        console.log('rut o validador vacios');
-
-      }
-      else{
-
-        var rutValidado = Fn.validaRut(rutCompleto);
-            if (rutValidado == false){
-              $('#errorRut').html('<span>Debe ingresar un Rut válido</span>').fadeIn(1000);
-              quitarPassword();
-              desactivarCaptcha();
-            }
-            else {
-
-             $('#errorRut').fadeOut('slow');
-
-             $.getJSON( "https://api.ipify.org?format=json", function(data) {
-                verificaRutIp(rutCompleto,data.ip)
-             })
-             .fail(function() {
-                $.ajax( "obtenerIp" )
-                      .done(function(ip) {
-                       verificaRutIp(rutCompleto,ip)
-                      });
-             });
-
-            } 
-      }
-    });
-  });
-}
-
-function verificaRutIp(rut,ip){
-
-cargaAnimacion();
-
-$.ajax({
-      async: true,
-      type: "POST",
-      url: url+"verificaRutIp",
-      dataType: 'json',
-      data: {
-        'ip': ip,
-        'rut': rut
-      },
-      success: function(respuesta){
-        quitarAnimacion();
-        console.log(respuesta);
-        $('#entrar').removeAttr("disabled");
-
-        if(respuesta.status=='validado'){
-              quitarPassword();
-              desactivarCaptcha();
-              $('#input-password').html('');
-              validaIngresoRut();
-        }
-        else{
-              console.log('no validado');
-              mostrarPassword();
-              $('#input-password').html(respuesta.formulario);
-              mostrarCaptcha();
-              validaIngresoCompleto(ip);
-        }
-      },
-      error: function(){
-        console.log('hay un error');
-      }
-    });
 
 }
-
